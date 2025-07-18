ce5eb8267aaa1fb65fe9f13bb6a21e9e41189c99
DM7935-GH <davidmuoneke45@gmail.com>
2025-07-18 19:12:10 +0100

    Replaced styles.css with styles.php

--- about.php
+++ about.php
@@ -10,12 +10,12 @@ session_start(); // Starts the PHP session
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>About Page</title>
+        <title>About VizuRank Lists</title>
 
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
-        <link rel=stylesheet type='text/css' href='./Protected/styles.css'>
+        <link rel=stylesheet type='text/css' href='./Protected/styles.php'>
 
         <style type="text/css">
 
@@ -23,10 +23,6 @@ session_start(); // Starts the PHP session
     </head>
 
     <body>
-        <?php
-        if(isset($_COOKIE['username'])) {
-            echo $_COOKIE['username'];
-        }
-        ?>
+        <h2>About VizuRank Lists</h2>
     </body>
 </html>
--- games.php
+++ games.php
@@ -15,7 +15,7 @@ session_start(); // Starts the PHP session
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
-        <link rel=stylesheet type='text/css' href='./Protected/styles.css'>
+        <link rel=stylesheet type='text/css' href='./Protected/styles.php'>
 
         <style type="text/css">
 
@@ -23,10 +23,6 @@ session_start(); // Starts the PHP session
     </head>
 
     <body>
-        <?php
-        if(isset($_COOKIE['username'])) {
-            echo $_COOKIE['username'];
-        }
-        ?>
+        <h2>List Games</h2>
     </body>
 </html>
