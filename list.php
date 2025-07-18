35593bf573582de2644d35bed71d87a4093d68b1
DM7935-GH <davidmuoneke45@gmail.com>
2025-07-18 16:44:54 +0100

    Pushed initial website files to the GitHub repository.

---
+++ list.php
@@ -0,0 +1,32 @@
+<?php
+session_name("TimeRank");
+session_start(); // Starts the PHP session
+?>
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>List Page</title>
+
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+        <link rel=stylesheet type='text/css' href='./Protected/styles.css'>
+
+        <style type="text/css">
+
+        </style>
+    </head>
+
+    <body>
+        <?php
+        if(isset($_COOKIE['username'])) {
+            echo $_COOKIE['username'];
+        }
+        ?>
+    </body>
+</html>
